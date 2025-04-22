09e1705d826a3445b47974c6911ef3a040f79853
Timeix3 <timeix3@gmail.com>
2025-04-22 22:21:00 +0300

    solved tasks 1,2,3

---
+++ ProjCharGenerator.Tests/Tests.cs
@@ -0,0 +1,103 @@
+using generator;
+
+namespace ProjCharGenerator.Tests
+{
+    public class Tests
+    {
+        string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+        [Fact]
+        public void Test1()
+        {
+            var exception = Assert.Throws<FileNotFoundException>(() => new BigramsGenerator(projectDirectory,"invalid.txt"));
+            Assert.Equal("File not found", exception.Message);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            BigramsGenerator generator = new(projectDirectory);
+            Assert.Equal(10, generator.size);
+            Assert.Equal(7598794, generator.summa);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            BigramsGenerator generator = new(projectDirectory);
+            Assert.Equal(917165, generator.GetWeight("ае"));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            BigramsGenerator generator = new(projectDirectory);
+            var bigrams = new[] { "аа", "аб", "ав", "аг", "ад", "ае", "аё", "аж", "аз", "аи" };
+            string result = generator.GetSym();
+            Assert.Contains(result, bigrams);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            BigramsGenerator generator = new(projectDirectory);
+            SortedDictionary<string, int> statistics = new();
+            for (int i = 0; i < 10000; i++)
+            {
+                string bigram = generator.GetSym();
+                if (statistics.ContainsKey(bigram))
+                    statistics[bigram]++;
+                else
+                    statistics.Add(bigram, 1);
+            }
+            Assert.Equal((double)generator.GetWeight("аа") / generator.summa, statistics["аа"] / 10000.0, 1);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            var exception = Assert.Throws<FileNotFoundException>(() => new WordsGenerator(projectDirectory, "invalid.txt"));
+            Assert.Equal("File not found", exception.Message);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            WordsGenerator generator = new(projectDirectory);
+            Assert.Equal(10, generator.size);
+            Assert.Equal(1655714, generator.summa);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            WordsGenerator generator = new(projectDirectory);
+            Assert.Equal(126844, generator.GetWeight("я"));
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            WordsGenerator generator = new(projectDirectory);
+            var words = new[] { "и", "в", "не", "на", "я", "быть", "он", "с", "что", "а" };
+            string result = generator.GetSym();
+            Assert.Contains(result, words);
+        }
+
+        [Fact]
+        public void Test10()
+        {
+            WordsGenerator generator = new(projectDirectory);
+            SortedDictionary<string, int> statistics = new();
+            for (int i = 0; i < 10000; i++)
+            {
+                string word = generator.GetSym();
+                if (statistics.ContainsKey(word))
+                    statistics[word]++;
+                else
+                    statistics.Add(word, 1);
+            }
+            Assert.Equal((double)generator.GetWeight("и") / generator.summa, statistics["и"] / 10000.0, 1);
+        }
+    }
+}
--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,44 +1,243 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ScottPlot;
 
 namespace generator
 {
-    class CharGenerator 
+    public class BigramsGenerator
     {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
-        private char[] data;
-        private int size;
+        private List<(string bigram, int weight)> data = new();
         private Random random = new Random();
-        public CharGenerator() 
+        public int size;
+        private int[] np;
+        public int summa = 0;
+        public BigramsGenerator(string projectDirectory, string fileName = "bigrams.txt")
         {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
+            if (!File.Exists(Path.Combine(projectDirectory, fileName)))
+                throw new FileNotFoundException("File not found");
+            using (StreamReader reader = new StreamReader(Path.Combine(projectDirectory, fileName)))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+                    data.Add((values[1], int.Parse(values[2])));
+                }
+            }
+            size = data.Count;
+            for (int i = 0; i < size; i++)
+                summa += data[i].weight;
+            np = new int[size];
+            int s2 = 0;
+            for (int i = 0; i < size; i++)
+            {
+                s2 += data[i].weight;
+                np[i] = s2;
+            }
         }
-        public char getSym() 
+        public string GetSym()
         {
-           return data[random.Next(0, size)]; 
+            int m = random.Next(0, summa);
+            int j;
+            for (j = 0; j < size; j++)
+            {
+                if (m < np[j])
+                    break;
+            }
+            return data[j].bigram;
+        }
+        public int GetWeight(string bigram) => data.Find(x => x.bigram == bigram).weight;
+
+        public void CreatePlot(List<KeyValuePair<string, int>> statistics, string projectDirectory)
+        {
+            var myPlot = new Plot();
+            int k = 1;
+            int barsCount = Math.Min(statistics.Count, 100);
+            for (int i = 0; i < barsCount; i++)
+            {
+                double[] positions = { k + 1, k + 1.8 };
+                double[] values = { statistics[i].Value / 1000.0, (double)GetWeight(statistics[i].Key) / summa };
+                var bars = myPlot.Add.Bars(positions, values);
+                bars.Bars[0].FillColor = Colors.Green;
+                bars.Bars[1].FillColor = Colors.Red;
+                k += 4;
+            }
+            ScottPlot.TickGenerators.NumericManual tickGen = new();
+            int j = 0;
+            for (int x = 1; x <= 4 * barsCount; x += 4)
+            {
+                tickGen.AddMajor(x + 1.5, statistics[j].Key);
+                j++;
+            }
+            myPlot.Axes.Bottom.TickGenerator = tickGen;
+            LegendItem expected = new()
+            {
+                LineColor = Colors.Red,
+                MarkerFillColor = Colors.Red,
+                MarkerLineColor = Colors.Red,
+                LineWidth = 4,
+                LabelText = "Expected frequency"
+            };
+            LegendItem actual = new()
+            {
+                LineColor = Colors.Green,
+                MarkerFillColor = Colors.Green,
+                MarkerLineColor = Colors.Green,
+                LineWidth = 4,
+                LabelText = "Actual frequency"
+            };
+            LegendItem[] legend = { expected, actual };
+            myPlot.ShowLegend(legend);
+            myPlot.Legend.Alignment = Alignment.UpperRight;
+            myPlot.Grid.XAxisStyle.IsVisible = false;
+            myPlot.Axes.Margins(bottom: 0);
+            myPlot.YLabel("Frequency");
+            myPlot.SavePng(Path.Combine(Directory.GetParent(projectDirectory).FullName, "Results/gen-1.png"), 2000, 500);
         }
     }
+
+    public class WordsGenerator
+    {
+        private List<(string word, int weight)> data = new();
+        private Random random = new Random();
+        public int size;
+        private int[] np;
+        public int summa = 0;
+        public WordsGenerator(string projectDirectory, string fileName = "words.txt")
+        {
+            if (!File.Exists(Path.Combine(projectDirectory, fileName)))
+                throw new FileNotFoundException("File not found");
+            using (StreamReader reader = new StreamReader(Path.Combine(projectDirectory, fileName)))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+                    data.Add((values[1], (int)(double.Parse(values[2]) * 10)));
+                }
+            }
+            size = data.Count;
+            for (int i = 0; i < size; i++)
+                summa += data[i].weight;
+            np = new int[size];
+            int s2 = 0;
+            for (int i = 0; i < size; i++)
+            {
+                s2 += data[i].weight;
+                np[i] = s2;
+            }
+        }
+        public string GetSym()
+        {
+            int m = random.Next(0, summa);
+            int j;
+            for (j = 0; j < size; j++)
+            {
+                if (m < np[j])
+                    break;
+            }
+            return data[j].word;
+        }
+        public int GetWeight(string word) => data.Find(x => x.word == word).weight;
+        public void CreatePlot(List<KeyValuePair<string, int>> statistics, string projectDirectory)
+        {
+            var myPlot = new Plot();
+            int k = 1;
+            int barsCount = Math.Min(statistics.Count, 100);
+            for (int i = 0; i < barsCount; i++)
+            {
+                double[] positions = { k + 1, k + 1.8 };
+                double[] values = { statistics[i].Value / 1000.0, (double)GetWeight(statistics[i].Key) / summa };
+                var bars = myPlot.Add.Bars(positions, values);
+                bars.Bars[0].FillColor = Colors.Green;
+                bars.Bars[1].FillColor = Colors.Red;
+                k += 4;
+            }
+            ScottPlot.TickGenerators.NumericManual tickGen = new();
+            int j = 0;
+            for (int x = 1; x <= 4 * barsCount; x += 4)
+            {
+                tickGen.AddMajor(x + 1.5, statistics[j].Key);
+                j++;
+            }
+            myPlot.Axes.Bottom.TickGenerator = tickGen;
+            LegendItem expected = new()
+            {
+                LineColor = Colors.Red,
+                MarkerFillColor = Colors.Red,
+                MarkerLineColor = Colors.Red,
+                LineWidth = 4,
+                LabelText = "Expected frequency"
+            };
+            LegendItem actual = new()
+            {
+                LineColor = Colors.Green,
+                MarkerFillColor = Colors.Green,
+                MarkerLineColor = Colors.Green,
+                LineWidth = 4,
+                LabelText = "Actual frequency"
+            };
+            int largestLabelWidth = statistics.MaxBy(x => x.Key.Length).Key.Length;
+            LegendItem[] legend = { expected, actual };
+            myPlot.ShowLegend(legend);
+            myPlot.Legend.Alignment = Alignment.UpperRight;
+            myPlot.Grid.XAxisStyle.IsVisible = false;
+            myPlot.Axes.Margins(bottom: 0);
+            myPlot.Axes.Bottom.TickLabelStyle.Rotation = -45;
+            myPlot.Axes.Bottom.TickLabelStyle.Alignment = Alignment.MiddleRight;
+            myPlot.Axes.Bottom.MinimumSize = largestLabelWidth * 8;
+            myPlot.YLabel("Frequency");
+            myPlot.SavePng(Path.Combine(Directory.GetParent(projectDirectory).FullName, "Results/gen-2.png"), 2000, 500);
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            CharGenerator gen = new CharGenerator();
-            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            GenerateBigramsText(projectDirectory);
+            GenerateWordsText(projectDirectory);
+        }
+
+        private static void GenerateBigramsText(string projectDirectory)
+        {
+            BigramsGenerator generator = new(projectDirectory);
+            StreamWriter writer = new(Path.Combine(Directory.GetParent(projectDirectory).FullName, "Results/gen-1.txt"));
+            SortedDictionary<string, int> statistics = new();
+            for (int i = 0; i < 1000; i++)
             {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
+                string bigram = generator.GetSym();
+                if (statistics.ContainsKey(bigram))
+                    statistics[bigram]++;
+                else
+                    statistics.Add(bigram, 1);
+                writer.Write(bigram);
             }
-            Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
+            writer.Close();
+            var sortedStatistics = statistics.OrderByDescending(entry => entry.Value).ToList();
+            generator.CreatePlot(sortedStatistics, projectDirectory);
+        }
+
+        private static void GenerateWordsText(string projectDirectory)
+        {
+            WordsGenerator generator = new(projectDirectory);
+            StreamWriter writer = new(Path.Combine(Directory.GetParent(projectDirectory).FullName, "Results/gen-2.txt"));
+            SortedDictionary<string, int> statistics = new();
+            for (int i = 0; i < 1000; i++)
             {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+                string word = generator.GetSym();
+                if (statistics.ContainsKey(word))
+                    statistics[word]++;
+                else
+                    statistics.Add(word, 1);
+                writer.Write(word + ' ');
             }
-            
+            writer.Close();
+            var sortedStatistics = statistics.OrderByDescending(entry => entry.Value).ToList();
+            generator.CreatePlot(sortedStatistics, projectDirectory);
         }
     }
 }
